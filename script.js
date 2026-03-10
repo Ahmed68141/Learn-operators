9cd58d664f3d35193b11401470e774a729c1acc8
Ahmed68141 <ahmedshaikh68141@gmail.com>
2026-03-10 15:00:54 +0100

    push

---
+++ script.js
@@ -0,0 +1,82 @@
+// Part 1: Arithmetic Operators (+ - * / % **)
+// Question 1
+let a = 5;
+let b = 2;
+
+console.log( a,"+", b ,"=",a + b);
+
+console.log( a,"-", b ,"=",a - b);
+console.log( a,"*", b ,"=",a * b);
+console.log( a,"/", b ,"=",a / b);
+console.log( a,"%", b ,"=",a % b);
+console.log( a,"**", b ,"=",a ** b);
+
+// Question 2
+
+let num = 6
+console.log(num,"**", 2 ,"= the square of" ,num ,"is",num ** 2);
+console.log(num,"**", 3,"= the cube of" ,num ,"is",num ** 3);
+
+// Question 3
+let e = 10;
+let f = 20;
+let g = 30;
+let sum =  e+f+g;
+console.log(e , "+", f , "+" , g ,"=" , e+f+g);
+console.log("the average of", e+f+g ,"is"  , sum / 3);
+
+// Part 2: Comparison Operators (== === != !== > < >= <=)
+
+// Question 4
+
+
+
+let c = 5
+let d = "5"
+
+console.log( c,"==", d,"=",c == d);
+console.log( c,"===",d ,"=",c === d);
+console.log( c,"!=", d,"=",c != d);
+console.log( c,"!==",d ,"=",c !== d);
+
+
+// Question 5
+
+let aa = 7;
+let bb = 5;
+
+console.log( aa,"<", bb,"=", aa < bb);
+console.log( aa,">",bb ,"=" , aa > bb);
+console.log( aa,"<=", bb,"=",  aa <= bb);
+console.log( aa,">=",bb ,"=" , aa >= bb);
+
+// Question 6
+
+    let n = 12
+    console.log(" n == ",n, "=" , n == 12);
+        console.log(" n == 12 =" , n == "12");
+    console.log(" n === ",n, "=" , n === 12);
+    console.log(" n === 12 =" , n === "12");
+
+
+//  Part 3: Assignment Operators (= += -= *= /=) 
+
+
+// Question 7
+let num2 = 15
+console.log(num2," += 5 =" ,num2 += 5);
+console.log(num2," -= 5 =" ,num2 -= 5);
+console.log(num2," *= 5 =" ,num2 *= 5);
+console.log(num2," /= 5 =" ,num2 /= 5);
+
+// Question 8
+
+let total = 200
+let t = 50+30+20;
+
+console.log("200 + 50 + 30 + 20 =" , total + t);
+
+
+
+
+
